f9324ef5a1696713337b2356df088f30e1d0bc28
Andreas Landerer <andreas.landerer@gmail.com>
2017-12-31 12:28:58 +0100

    Incorporated more robust mongo connection routine

--- app.js
+++ app.js
@@ -8,18 +8,19 @@ const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
 const MONGO_URL = process.env.MONGO_DB || 'mongodb://localhost:27017/pokemonDB';
 
-var server;
+const connectWithRetry = function() {
+    return mongoose.connect(MONGO_URL, function(err) {
+        if (err) {
+            console.error('Failed to connect to mongo on startup - retrying in 5 sec', err);
+            setTimeout(connectWithRetry, 5000);
+        } else {
+            app.listen(PORT, () => {
+                console.log(`Server successfully started at port ${PORT}!`);
+            });
+        }
+    });
+};
+connectWithRetry();
 
 app.use('/', pokemonRouter);
-mongoose.connect(MONGO_URL).catch((e) => {
-    console.error(e);
-    if(server) {
-        console.error('Shutting down server...')
-        server.close();
-    }
-});
-
-server = app.listen(PORT, () => {
-    console.log(`Server successfully started at port ${PORT}!`);
-});
 
